8e3dc5c1cc1994973a71a0868368f064c91109cc
Catzzz <chuneleu@gmail.com>
2021-05-15 15:51:49 -0700

    import seedColors.js, create Palette.js

---
+++ src/Palette.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                {/* {Navbar goes here} */}
+                <div className="Palteet-colors"></div>
+                {/* {bunch of colro boxes} */}
+                
+            </div>
+        )
+    }
+}
+
+export default Palette;
