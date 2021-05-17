b1868e109796e29c65451d945169f4c5f610e634
Catzzz <chuneleu@gmail.com>
2021-05-17 12:31:01 -0700

    add singleColorPalette.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 import { Route, Switch } from "react-router-dom";
 import PaletteList from './PaletteList';
+import SingleColorPalette from './SingleColorPalette';
 
 class App extends Component {
   findPalette(id) {
@@ -18,15 +19,15 @@ class App extends Component {
         <Route
           exact
           path="/palette/:id"
-          render={(routeParams) => (
+          render={(routeProps) => (
             <Palette
               palette={generatePalette(
-                this.findPalette(routeParams.match.params.id)
+                this.findPalette(routeProps.match.params.id)
               )}
             />
           )}
         />
-        <Route exact path="/palette/:paletteId/:colorId" render={ () => (<h1> Signle </h1>)}/>
+        <Route exact path="/palette/:paletteId/:colorId" render={ () => (<SingleColorPalette />)}/>
       </Switch>
 
       // <div className="App">
