73e0c645396e1c761736a90ce6acbca738b52985
Catzzz <chuneleu@gmail.com>
2021-05-17 20:01:40 -0700

    Move out style

--- src/Palette.js
+++ src/Palette.js
@@ -1,36 +1,13 @@
 import React, { Component } from 'react'
 import { withStyles } from "@material-ui/styles";
-import './Palette.css';
+
 import ColorBox from "./ColorBox";
 
 import Navbar from './Navbar';
 import PaletteFooter from './PaletteFooter';
+import styles from './styles/PaletteStyles';
 
 
-const styles = {
-    Palette:{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-    },
-    
-    PaletteColors:{
-        height: "90%",
-    },
-    
-    PaletteFooter:{
-        backgroundColor: "white",
-        height:"5vh",
-        display: "flex",
-        justifyContent: "flex-end",
-        alignItems: "center",
-        fontWeight: "bold",
-    },
-    emoji:{
-        fontSize: "1.5rem",
-        margin: "0 1rem",
-    },
-};
 
 class Palette extends Component {
     constructor(props){
---
+++ src/styles/PaletteListStyles.js
@@ -0,0 +1,37 @@
+
+export default {
+    root: {
+      backgroundColor: "blue",
+      height:"100vh",
+      display:"flex",
+      alignItems:"flex-start",
+      justifyContent:"center"
+      
+    },
+    container:{
+      with:"50%",
+      display:"flex",
+      alignItems:"flex-start",
+      flexDirection:"column",
+      flexWrap:"wrap",
+      // border:"1px solid white"
+  
+    },
+    nav:{
+      // backgroundColor:"pink",
+      display:"flex",
+      width:"100%",
+      justifyContent:"space-between",
+      color:"white"
+  
+    },
+    palettes:{
+      //backgroundColor:"green",
+      boxSizing:"border-box",
+      width:"100%",
+      display:"grid",
+      gridTemplateColumns:"repeat(3,30%)",
+      gridGap:"5%"
+    }
+  
+  };
